c1bb06ee09e78ddb45ef726fd9bd9b392f402491
Max Bebök <beboek.max@gmail.com>
2024-11-02 20:00:24 +0100

    chg: add fire part. sim

--- examples/18_particles/main.c
+++ examples/18_particles/main.c
@@ -8,19 +8,14 @@
 
 #define RCP_TICKS_TO_USECS(ticks) (((ticks) * 1000000ULL) / RCP_FREQUENCY)
 
-void generateParticles(TPXParticle *particles, int count, bool dynScale) {
+static void generateParticles(TPXParticle *particles, int count) {
   for (int i = 0; i < count; i++) {
     int p = i / 2;
     int8_t *ptPos = i % 2 == 0 ? particles[p].posA : particles[p].posB;
     uint8_t *ptColor = i % 2 == 0 ? particles[p].colorA : particles[p].colorB;
 
-    if(dynScale) {
-      particles[p].sizeA = 20 + (rand() % 10);
-      particles[p].sizeB = 20 + (rand() % 10);
-    } else {
-      particles[p].sizeA = 4;
-      particles[p].sizeB = 4;
-    }
+    particles[p].sizeA = 20 + (rand() % 10);
+    particles[p].sizeB = 20 + (rand() % 10);
 
     T3DVec3 pos = {{
        (i * 1 + rand()) % 64 - 32,
@@ -34,10 +29,6 @@ void generateParticles(TPXParticle *particles, int count, bool dynScale) {
     pos.v[1] *= len;
     pos.v[2] *= len;
 
-//    ptPos[0] = pos.v[0];
-//    ptPos[1] = pos.v[1];
-//    ptPos[2] = pos.v[2];
-
     ptPos[0] = (rand() % 256) - 128;
     ptPos[1] = (rand() % 256) - 128;
     ptPos[2] = (rand() % 256) - 128;
@@ -46,6 +37,66 @@ void generateParticles(TPXParticle *particles, int count, bool dynScale) {
     ptColor[0] = 25 + (rand() % 230);
     ptColor[1] = 25 + (rand() % 230);
     ptColor[2] = 25 + (rand() % 230);
+  }
+}
+
+static void gradient_fire(uint8_t *color, float t) {
+    //t = (1.0f - t);
+    t = fminf(1.0f, fmaxf(0.0f, t));
+
+    if (t < 0.25f) {
+        // Dark red to bright red
+        color[0] = (uint8_t)(200 * (t / 0.25f)) + 55;
+        color[1] = 0;
+        color[2] = 0;
+    } else if (t < 0.5f) {
+        // Bright red to yellow
+        color[0] = 255;
+        color[1] = (uint8_t)(255 * ((t - 0.25f) / 0.25f));
+        color[2] = 0;
+    } else if (t < 0.75f) {
+        // Yellow to white (optional, if you want a bright white center)
+        color[0] = 255;
+        color[1] = 255;
+        color[2] = (uint8_t)(255 * ((t - 0.5f) / 0.25f));
+    } else {
+        // White to black
+        color[0] = (uint8_t)(255 * (1.0f - (t - 0.75f) / 0.25f));
+        color[1] = (uint8_t)(255 * (1.0f - (t - 0.75f) / 0.25f));
+        color[2] = (uint8_t)(255 * (1.0f - (t - 0.75f) / 0.25f));
+    }
+}
+
+static int currentPart  = 0;
+
+static void simulate_particles(TPXParticle *particles, int partCount, float posX, float posZ) {
+  int p = currentPart / 2;
+  if(currentPart % (1+(rand() % 3)) == 0) {
+    int8_t *ptPos = currentPart % 2 == 0 ? particles[p].posA : particles[p].posB;
+    int8_t *size = currentPart % 2 == 0 ? &particles[p].sizeA : &particles[p].sizeB;
+    uint8_t *color = currentPart % 2 == 0 ? particles[p].colorA : particles[p].colorB;
+
+    ptPos[0] = posX + (rand() % 16) - 8;
+    ptPos[1] = -126;
+    gradient_fire(color, 0);
+    ptPos[2] = posZ + (rand() % 16) - 8;
+    *size = 60 + (rand() % 10);
+  }
+  currentPart = (currentPart + 1) % partCount;
+
+  // move all up by one unit
+  for (int i = 0; i < partCount/2; i++) {
+    gradient_fire(particles[i].colorA, (particles[i].posA[1] + 127) / 150.0f);
+    gradient_fire(particles[i].colorB, (particles[i].posB[1] + 127) / 150.0f);
+
+    particles[i].posA[1] += 1;
+    particles[i].posB[1] += 1;
+    if(currentPart % 4 == 0) {
+      particles[i].sizeA -= 2;
+      particles[i].sizeB -= 2;
+      if(particles[i].sizeA < 0)particles[i].sizeA = 0;
+      if(particles[i].sizeB < 0)particles[i].sizeB = 0;
+    }
   }
 }
 
@@ -90,13 +141,14 @@ int main()
   float camRotX = 1.5;
   float camRotY = 4.0f;
   bool showModel = false;
+  int example = 0;
 
   int batchSize = 344;
   int batchCountMax = 500;
-  int batchCount = 100;
+  int batchCount = 2;
   int particleCount = batchSize * batchCountMax;
   TPXParticle *particles = malloc_uncached(sizeof(TPXParticle) * particleCount/2);
-  generateParticles(particles, particleCount, true);
+  generateParticles(particles, particleCount);
 
   uint8_t colorAmbient[4] = {0xFF, 0xFF, 0xFF, 0xFF};
   T3DVec3 lightDirVec = {{0.0f, 0.0f, 1.0f}};
@@ -106,6 +158,9 @@ int main()
   float rotAngle = 0.0f;
   float partScale = 0.25f;
   float partSize = 0.15f;
+
+  T3DVec3 flameScale;
+  T3DVec3 flamePos = {{0,0,0}};
 
   for(;;)
   {
@@ -130,10 +185,14 @@ int main()
     if(batchCount < 1)batchCount = 1;
     if(batchCount > batchCountMax)batchCount = batchCountMax;
 
-    particleCount = batchSize * batchCount;
+    if(btn.c_left || btn.c_right) {
+      example = example == 1 ? 0 : 1;
+      generateParticles(particles, particleCount);
+    }
 
     if(btn.start)showModel = !showModel;
     {
+      float flameSpeed = deltaTime * 2.1f;
       float camSpeed = deltaTime * 1.1f;
       float camRotSpeed = deltaTime * 0.02f;
 
@@ -146,12 +205,20 @@ int main()
         camRotX += (float)joypad.stick_x * camRotSpeed;
         camRotY += (float)joypad.stick_y * camRotSpeed;
       } else {
-        camPos.v[0] += camDir.v[0] * (float)joypad.stick_y * camSpeed;
-        camPos.v[1] += camDir.v[1] * (float)joypad.stick_y * camSpeed;
-        camPos.v[2] += camDir.v[2] * (float)joypad.stick_y * camSpeed;
+        if(joypad.btn.l) {
+          flamePos.v[0] += camDir.v[0] * (float)joypad.stick_y * flameSpeed;
+          flamePos.v[2] += camDir.v[2] * (float)joypad.stick_y * flameSpeed;
+          flamePos.v[0] += camDir.v[2] * (float)joypad.stick_x * -flameSpeed;
+          flamePos.v[2] -= camDir.v[0] * (float)joypad.stick_x * -flameSpeed;
 
-        camPos.v[0] += camDir.v[2] * (float)joypad.stick_x * -camSpeed;
-        camPos.v[2] -= camDir.v[0] * (float)joypad.stick_x * -camSpeed;
+        } else {
+          camPos.v[0] += camDir.v[0] * (float)joypad.stick_y * camSpeed;
+          camPos.v[1] += camDir.v[1] * (float)joypad.stick_y * camSpeed;
+          camPos.v[2] += camDir.v[2] * (float)joypad.stick_y * camSpeed;
+
+          camPos.v[0] += camDir.v[2] * (float)joypad.stick_x * -camSpeed;
+          camPos.v[2] -= camDir.v[0] * (float)joypad.stick_x * -camSpeed;
+        }
       }
 
       if(joypad.btn.c_up)camPos.v[1] += camSpeed * 45.0f;
@@ -162,6 +229,17 @@ int main()
       camTarget.v[2] = camPos.v[2] + camDir.v[2];
     }
 
+    if(example == 1) {
+      //batchCount = 2;
+      rotAngle = 0;
+      simulate_particles(particles, batchSize * batchCount, flamePos.v[0], flamePos.v[2]);
+      flameScale = (T3DVec3){{0.7f, partScale, 0.7f}};
+    } else {
+      flameScale = (T3DVec3){{partScale, partScale, partScale}};
+    }
+
+    particleCount = batchSize * batchCount;
+
     // we can set up our viewport settings beforehand here
     t3d_viewport_set_projection(&viewport, T3D_DEG_TO_RAD(80.0f), 5.0f, 250.0f);
     t3d_viewport_look_at(&viewport, &camPos, &camTarget, &(T3DVec3){{0,1,0}});
@@ -169,6 +247,9 @@ int main()
     // ======== Draw (3D) ======== //
     rdpq_attach(display_get(), display_get_zbuf()); // set the target to draw to
     t3d_frame_start(); // call this once per frame at the beginning of your draw function
+
+    rdpq_mode_antialias(AA_NONE);
+    rdpq_mode_dithering(DITHER_NONE_NONE);
 
     t3d_viewport_attach(&viewport); // now use the viewport, this applies proj/view matrices and sets scissoring
 
@@ -179,15 +260,15 @@ int main()
     t3d_light_set_count(0);
 
     t3d_mat4fp_from_srt_euler(matPartFP,
-      (float[]){partScale, partScale, partScale},
+      flameScale.v,
       (float[]){rotAngle,rotAngle*2.4f,rotAngle*1.3f},
-      (float[]){0,0,0}
+      (float[]){0, example == 1 ? (100*partScale) : 0, 0}
     );
     float modelScale = 0.25f;
     t3d_mat4fp_from_srt_euler(matFP,
       (float[]){modelScale, modelScale, modelScale},
       (float[]){0,0,0},
-      (float[]){0,0,0}
+      (float[]){0,-10,0}
     );
     t3d_matrix_push(matFP);
     if(showModel)rspq_block_run(model->userBlock);
@@ -202,12 +283,9 @@ int main()
     tpx_state_from_t3d(); // copy the current state from t3d to tpx
     tpx_state_set_scale(partSize);
 
-    //uint64_t ticks = get_ticks();
     for(int i = 0; i < batchCount; i++) {
       tpx_particle_draw(particles + (i * batchSize/2), batchSize);
     }
-    //rspq_wait();
-    //ticks = get_ticks() - ticks;
 
     t3d_matrix_pop(1);
 
@@ -219,7 +297,8 @@ int main()
 
     #if RSPQ_PROFILE
       double timePerPart = (double)rspTimeTPX / (double)particleCount;
-      rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 24, 240-34, "RSP/tpx: %lldus (%.4f)", rspTimeTPX, timePerPart);
+      //rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 24, 240-34, "RSP/tpx: %lldus (%.4f)", rspTimeTPX, timePerPart);
+      rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 24, 240-34, "RSP/tpx: %lldus", rspTimeTPX);
       rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 24, 240-24, "RDP    : %lldus", rdpTimeBusy);
     #endif
 //
