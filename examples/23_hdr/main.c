860f40c458d55b8485c40e3bc6246e7d2fd9941c
Max Bebök <beboek.max@gmail.com>
2025-04-21 23:50:22 +0200

    chg: add HDR example to build scripts, tidy up demo code

--- examples/23_hdr/main.c
+++ examples/23_hdr/main.c
@@ -4,8 +4,21 @@
 #include <t3d/t3d.h>
 #include <t3d/t3dmodel.h>
 #include <t3d/t3ddebug.h>
-
 #include "debug_overlay.h"
+
+/**
+ * Example project showcasing HDR rendering.
+ * This will both use a special ucode feature as well as a specific material setup.
+ *
+ * Tiny3D allows for an exposure setting, which will simply scale the final color (vertex+lighting)
+ * before passing it to the RDP.
+ * If color gets scaled above 1.0 it will be clamped by the ucode.
+ * In contrast the RDP, where you can only add or scale down, this allows for higher amplifications of color.
+ * Also avoiding the usual overflow in the process.
+ * Once that clamped color reaches the RDP, it can be used in a special CC setup to simulate a bloom effect.
+ *
+ * Special thanks to https://github.com/SpookyIluha for providing this demo!
+ */
 
 surface_t* fb = NULL;
 
@@ -48,12 +61,7 @@ void exposure_set(void* framebuffer){
   if(exposure < 0) exposure = 0;
 }
 
-
-/**
- * Simple example with a spinning quad.
- * This shows how to manually generate geometry and draw it,
- * although most o the time you should use the builtin model format.
- */
+[[noreturn]]
 int main()
 {
   profile_data.frame_count = 0;
@@ -67,8 +75,6 @@ int main()
 
   rdpq_init();
   rspq_profile_start();
-  //rdpq_debug_start();
-  //rdpq_debug_log(true);
 
   joypad_init();
   t3d_init((T3DInitParams){});
@@ -76,19 +82,26 @@ int main()
   viewport[0] = t3d_viewport_create();
   viewport[1] = t3d_viewport_create();
   viewport[2] = t3d_viewport_create();
-  //viewport.guardBandScale = 1;
 
   t3d_debug_print_init();
   sprite_t *spriteLogo = sprite_load("rom:/logo.ia8.sprite");
   T3DModel *model = t3d_model_load("rom://arcvis_baked_282.t3dm");
-  T3DModelState matstate = t3d_model_state_create();
   rdpq_font_t* font = rdpq_font_load_builtin(FONT_BUILTIN_DEBUG_MONO);
   rdpq_text_register_font(1, font);
 
-  T3DMat4 modelMat; // matrix for our model, this is a "normal" float matrix
-  t3d_mat4_identity(&modelMat);
-  // Now allocate a fixed-point matrix, this is what t3d uses internally.
   T3DMat4FP* modelMatFP = malloc_uncached(sizeof(T3DMat4FP));
+  float modelScale = 0.15f;
+  t3d_mat4fp_from_srt_euler(modelMatFP,
+    (float[3]){modelScale, modelScale, modelScale},
+    (float[3]){0,0,0},
+    (float[3]){0,0,0}
+  );
+
+  rspq_block_begin();
+    t3d_matrix_push(modelMatFP);
+    t3d_model_draw(model);
+    t3d_matrix_pop(1);
+  model->userBlock = rspq_block_end();
 
   T3DVec3 camPos = {{0.0, 0.0, 0.0}};
   T3DVec3 camTarget = {{0,0,0}};
@@ -97,44 +110,24 @@ int main()
   float camRotX = 3.14f;
   float camRotY = 0.24f;
 
-  uint8_t colorAmbient[4] = {50, 50, 50, 0xFF};
-  //uint8_t colorAmbient[4] = {40, 40, 40, 0xFF};
-  uint8_t colorDir[4]     = {0xFF, 0xFF, 0xFF, 0xFF};
-
-  T3DVec3 lightDirVec = {{0.0f, 1.0f, 1.0f}};
-  t3d_vec3_norm(&lightDirVec);
-
-  float rotAngle = 0.0f;
-  T3DVec3 rotAxis = {{1.0f, 2.5f, 0.25f}};
-  t3d_vec3_norm(&rotAxis);
-
-  double lastTimeMs = 0;
-  float time = 0.0f;
+  uint8_t colorAmbient[4] = {100, 100, 100, 0xFF};
 
   bool requestDisplayMetrics = false;
   bool displayMetrics = false;
   float last3dFPS = 0.0f;
 
-  float vertFxTime = 0;
-  int vertFxFunc = T3D_VERTEX_FX_NONE;
   for(uint64_t frame = 0;; ++frame)
   {
     joypad_poll();
     joypad_inputs_t joypad = joypad_get_inputs(JOYPAD_PORT_1);
-    joypad_buttons_t btn = joypad_get_buttons_pressed(JOYPAD_PORT_1);
     if(joypad.stick_x < 10 && joypad.stick_x > -10)joypad.stick_x = 0;
     if(joypad.stick_y < 10 && joypad.stick_y > -10)joypad.stick_y = 0;
 
-    double nowMs = (double)get_ticks_us() / 1000.0;
-    float deltaTime = (float)(nowMs - lastTimeMs);
-    lastTimeMs = nowMs;
-    time += deltaTime;
-
-    vertFxTime = fmaxf(vertFxTime - deltaTime, 0.0f);
+    float deltaTime = display_get_delta_time();
 
     {
-      float camSpeed = deltaTime * 0.0003f;
-      float camRotSpeed = deltaTime * 0.00001f;
+      float camSpeed = deltaTime * 0.3f;
+      float camRotSpeed = deltaTime * 0.01f;
 
       camDir.v[0] = fm_cosf(camRotX) * fm_cosf(camRotY);
       camDir.v[1] = fm_sinf(camRotY);
@@ -163,42 +156,13 @@ int main()
       camTarget.v[2] = camPos.v[2] + camDir.v[2];
     }
 
-    color_t fogColor = (color_t){0xFF, 0xFF, 0xFF, 0xFF};
-
     if(joypad.btn.b)
     {
       requestDisplayMetrics = true;
-      colorAmbient[2] = colorAmbient[1] = colorAmbient[0] = 40;
-      colorDir[2] = colorDir[1] = colorDir[0] = 40;
-      fogColor = (color_t){0x00, 0x00, 0x00, 0xFF};
     } else {
       requestDisplayMetrics = false;
       displayMetrics = false;
-
-      colorAmbient[2] = colorAmbient[1] = colorAmbient[0] = 100;
-      colorDir[2] = colorDir[1] = colorDir[0] = 0xFF;
-
-      // roate light around axis
-      lightDirVec.v[0] = fm_cosf(time * 0.002f);
-      lightDirVec.v[1] = 0.0f;//fm_sinf(time * 0.002f);
-      lightDirVec.v[2] = fm_sinf(time * 0.002f);
-      t3d_vec3_norm(&lightDirVec);
     }
-
-    if(btn.l) {
-      vertFxTime = 500.0f;
-      vertFxFunc++;
-      if(vertFxFunc > T3D_VERTEX_FX_OUTLINE)vertFxFunc = T3D_VERTEX_FX_NONE;
-      t3d_state_set_vertex_fx(vertFxFunc, 32, 32);
-    }
-
-    rotAngle += 0.03f;
-
-    float modelScale = 0.15f;
-    t3d_mat4_identity(&modelMat);
-    //t3d_mat4_rotate(&modelMat, &rotAxis, rotAngle);
-    t3d_mat4_scale(&modelMat, modelScale, modelScale, modelScale);
-    t3d_mat4_to_fixed(modelMatFP, &modelMat);
 
     t3d_viewport_set_projection(&viewport[(frame + 1) % 3], T3D_DEG_TO_RAD(85.0f), 2.5f, 100.0f);
     t3d_viewport_look_at(&viewport[(frame + 1) % 3], &camPos, &camTarget, &(T3DVec3){{0,1,0}});
@@ -211,33 +175,16 @@ int main()
     t3d_viewport_attach(&viewport[(frame) % 3]);
     t3d_screen_clear_depth();
 
-    t3d_light_set_ambient(colorAmbient); // one global ambient light, always active, we have baked vertex colors in this scene
+    t3d_light_set_ambient(colorAmbient);
     t3d_light_set_count(0);
     t3d_light_set_exposure(exposure);
 
-    // t3d functions can be recorded into a display list:
-    if(!model->userBlock) {
-        rspq_block_begin();
-        t3d_model_draw(model);
-        model->userBlock = rspq_block_end();
-    }
     rdpq_sync_pipe();
-    t3d_matrix_push(modelMatFP);
     rspq_block_run(model->userBlock);
-    t3d_matrix_pop(1);
     rdpq_sync_pipe();
-
-    if(vertFxTime > 0.0f) {
-      rdpq_text_printf(NULL, 1, 16, 16, "VertexFX: %i", vertFxFunc);
-    }
 
     if(displayMetrics)
     {
-
-      // show pos / rot
-      //debug_printf_screen(24, 190, "Pos: %.4f %.4f %.4f", camPos.v[0], camPos.v[1], camPos.v[2]);
-      //t3d_debug_printf(24, 200, "Rot: %.4f %.4f", camRotX, camRotY);
-
       if(profile_data.frame_count == 0) {
         rdpq_text_printf(NULL, 1, 140, 206, "FPS (3D)   : %.4f", last3dFPS);
         rdpq_text_printf(NULL, 1, 140, 218, "FPS (3D+UI): %.4f", display_get_fps());
@@ -273,7 +220,4 @@ int main()
       rspq_profile_reset();
     }
   }
-
-  t3d_destroy();
-  return 0;
 }
